3c018d67ba52f30b9aff70e6497b851e2c96283e
Nikhil Marathe <me@nikhilism.com>
2024-05-21 22:10:57 -0700

    do it properly this time

--- info.rkt
+++ info.rkt
@@ -1,6 +1,6 @@
 #lang info
 (define collection "postal")
-(define deps '("base", "binaryio-lib", "net-ip-lib"))
+(define deps '("base" "binaryio-lib" "net-ip-lib"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/postal.scrbl" ())))
 (define pkg-desc "A DHCP client")
